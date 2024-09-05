ed0a21a5446bd2c64cfb24e95308d64822c522e8
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-05 14:20:48 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -3,7 +3,7 @@
 
 // Define the mapping for Soundex codes
 const char soundexMapping[26] = {
-    '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', // A-J 
+    '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', // A-J
     '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', // K-T
     '0', '1', '0', '2', '0', '2'  // U-Z
 };
@@ -17,19 +17,22 @@ char getSoundexCode(char c) {
     return '0'; // For non-alphabet characters
 }
 
+// Function to fill the remaining Soundex string with zeros
 void fillRemainingWithZeros(char *soundex, int *sIndex) {
     while (*sIndex < 4) {
-        soundex[(*sIndex)++] = '0'; // Fill remaining with zeros
+        soundex[(*sIndex)++] = '0'; // Fill remaining positions with zeros
     }
-    soundex[4] = '\0'; // Null terminate the string
+    soundex[4] = '\0'; // Null-terminate the Soundex string
 }
 
+// Function to add a character to the Soundex code if it's valid
 void addCharacterIfValid(char code, char previousCode, char *soundex, int *sIndex) {
-    if (shouldAddCode(code, previousCode)) {
+    if (code != '0' && code != previousCode) { // Check if the code should be added
         soundex[(*sIndex)++] = code;
     }
 }
 
+// Function to process the remaining characters in the name
 void processCharacter(const char *name, char *soundex, int *sIndex, int len) {
     for (int i = 1; i < len && *sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
@@ -37,12 +40,12 @@ void processCharacter(const char *name, char *soundex, int *sIndex, int len) {
     }
 }
 
-
+// Function to generate the Soundex code for a given name
 void generateSoundex(const char *name, char *soundex) {
-    soundex[0] = toupper(name[0]); // Set the first letter
-    int sIndex = 1;
-    int len = strlen(name);
+    soundex[0] = toupper(name[0]); // Set the first letter of the Soundex code
+    int sIndex = 1; // Start index for the Soundex code
+    int len = strlen(name); // Length of the input name
 
-    processNameCharacters(name, soundex, &sIndex, len);
-    fillRemainingWithZeros(soundex, &sIndex);
+    processCharacter(name, soundex, &sIndex, len); // Process the remaining characters in the name
+    fillRemainingWithZeros(soundex, &sIndex); // Ensure the Soundex code has 4 characters
 }
