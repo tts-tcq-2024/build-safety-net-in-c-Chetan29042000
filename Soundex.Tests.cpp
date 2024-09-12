473b221eb9fd299cc50e8f2b74c99584bcdbc3a0
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-12 11:59:42 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -6,32 +6,32 @@
 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  char soundex[5];
- // Testcase 1: Common name
- generateSoundex("Robert", soundex); 
- ASSERT_STREQ(soundex,"R163");
- 
- // Testcase 2: Name with different consonants
- generateSoundex("Ashcraft", soundex);
- ASSERT_STREQ(soundex,"A261");
- 
- // Testcase 3: Single character
- generateSoundex("A", soundex);
- ASSERT_STREQ(soundex,"A000");
+// Testcase 1: Common Name Soundex Conversion
+generateSoundex("Robert", soundex);
+ASSERT_STREQ(soundex, "R163");
 
- // Testcase 4: name Starts with vowel
- generateSoundex("Euler", soundex);
- ASSERT_STREQ(soundex,"E460");
- 
-// Testcase 5: Short name with no changes
- generateSoundex("Lee", soundex);
- ASSERT_STREQ(soundex,"L000");
+// Testcase 2: Name with Different Consonants Soundex Conversion
+generateSoundex("Ashcraft", soundex);
+ASSERT_STREQ(soundex, "A261");
 
- // Testcase 6: Name with special character '@'
- generateSoundex("B@rton", soundex);
- ASSERT_STREQ(soundex,"B635");
+// Testcase 3: Single Character Name Soundex Conversion
+generateSoundex("A", soundex);
+ASSERT_STREQ(soundex, "A000");
 
- // Testcase 7: Name with repeating letter
- generateSoundex("Addams", soundex);
- ASSERT_STREQ(soundex,"A352");
+// Testcase 4: Name Starting with a Vowel Soundex Conversion
+generateSoundex("Euler", soundex);
+ASSERT_STREQ(soundex, "E460");
+
+// Testcase 5: Short Name with No Changes Soundex Conversion
+generateSoundex("Lee", soundex);
+ASSERT_STREQ(soundex, "L000");
+
+// Testcase 6: Name with Special Character '@' in Soundex Conversion
+generateSoundex("B@rton", soundex);
+ASSERT_STREQ(soundex, "B635");
+
+// Testcase 7: Name with Repeating Letters Soundex Conversion
+generateSoundex("Addams", soundex);
+ASSERT_STREQ(soundex, "A352");
  
 }
