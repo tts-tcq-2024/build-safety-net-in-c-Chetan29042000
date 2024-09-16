d7624254e7988fba357c6d6e6b0fb3f16ba89bbb
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-16 10:41:29 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,34 +4,51 @@
 #include <string.h>
 #include <assert.h>
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- char soundex[5];
 // Testcase 1: Common Name Soundex Conversion
-generateSoundex("Robert", soundex);
-ASSERT_STREQ(soundex, "R163");
+TEST(SoundexTestsuite, HandlesCommonNameCorrectly) {
+    char soundex[5];
+    generateSoundex("Robert", soundex);
+    ASSERT_STREQ(soundex, "R163");
+}
 
 // Testcase 2: Name with Different Consonants Soundex Conversion
-generateSoundex("Ashcraft", soundex);
-ASSERT_STREQ(soundex, "A261");
+TEST(SoundexTestsuite, HandlesNameWithDifferentConsonantsCorrectly) {
+    char soundex[5];
+    generateSoundex("Ashcraft", soundex);
+    ASSERT_STREQ(soundex, "A261");
+}
 
 // Testcase 3: Single Character Name Soundex Conversion
-generateSoundex("A", soundex);
-ASSERT_STREQ(soundex, "A000");
+TEST(SoundexTestsuite, HandlesSingleCharacterNameCorrectly) {
+    char soundex[5];
+    generateSoundex("A", soundex);
+    ASSERT_STREQ(soundex, "A000");
+}
 
 // Testcase 4: Name Starting with a Vowel Soundex Conversion
-generateSoundex("Euler", soundex);
-ASSERT_STREQ(soundex, "E460");
+TEST(SoundexTestsuite, HandlesNameStartingWithVowelCorrectly) {
+    char soundex[5];
+    generateSoundex("Euler", soundex);
+    ASSERT_STREQ(soundex, "E460");
+}
 
 // Testcase 5: Short Name with No Changes Soundex Conversion
-generateSoundex("Lee", soundex);
-ASSERT_STREQ(soundex, "L000");
+TEST(SoundexTestsuite, HandlesShortNameWithNoChangesCorrectly) {
+    char soundex[5];
+    generateSoundex("Lee", soundex);
+    ASSERT_STREQ(soundex, "L000");
+}
 
 // Testcase 6: Name with Special Character '@' in Soundex Conversion
-generateSoundex("B@rton", soundex);
-ASSERT_STREQ(soundex, "B635");
+TEST(SoundexTestsuite, HandlesNameWithSpecialCharacterCorrectly) {
+    char soundex[5];
+    generateSoundex("B@rton", soundex);
+    ASSERT_STREQ(soundex, "B635");
+}
 
 // Testcase 7: Name with Repeating Letters Soundex Conversion
-generateSoundex("Addams", soundex);
-ASSERT_STREQ(soundex, "A352");
- 
+TEST(SoundexTestsuite, HandlesNameWithRepeatingLettersCorrectly) {
+    char soundex[5];
+    generateSoundex("Addams", soundex);
+    ASSERT_STREQ(soundex, "A352");
 }
